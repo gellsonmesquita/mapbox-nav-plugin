9fb9df202e250238c89bb5cf7046444439f8b940
gellsonmesquita <iamgelson@gmail.com>
2026-01-08 03:07:36 +0100

    refactor: remove hardcoded Mapbox access token from NavigationScreen

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -161,7 +161,6 @@ class _NavigationScreenState extends State<NavigationScreen> {
                 color: Colors.grey[200],
                 child: MapboxNavigationView(
                   onMapboxViewCreated: _onMapboxViewCreated, // Passe o callback
-                  accessToken: 'pk.eyJ1IjoicmVmcmlhbmdvIiwiYSI6ImNsejlsajczMjBhdHoyanBqZTc4dDU2aWwifQ.ovcPssyrDzf3Fp9w-XllEA',
                 ),
               ),
             ),
